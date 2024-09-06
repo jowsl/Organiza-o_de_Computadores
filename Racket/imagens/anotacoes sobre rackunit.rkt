35997c51cafbd4dfff783d981d8979b73e6eb459
jowsl <154270063+jowsl@users.noreply.github.com>
2024-09-06 08:12:54 -0300

    Delete Racket directory

--- Racket/imagens/anotacoes sobre rackunit.rkt
+++
@@ -1,7 +0,0 @@
-#lang racket
-(require rackunit) ;; biblioteca para usar comandos de teste como
-#| Esses teste vão no painel de interatividade.
-check-equal?, check-not-equal?, check-pred recebe um predicado e argumento.
-check-= verifica se dois números estão em uma certa distancia um do outro.
-check-true, check-false, check-not-false.
-|#
